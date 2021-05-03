edf9565f02d7cc7097dd46de7bca62a706eeae62
AshwinGhub <49360364+AshwinGhub@users.noreply.github.com>
2021-05-03 14:41:11 +0530

    Changed object name for core_top instantiation

--- test_core.v
+++ test_core.v
@@ -3,7 +3,7 @@ module test_core();
 reg clk, reset;
 
 core_top #(.PMA_SIZE(16), .PMD_SIZE(32), .DMA_SIZE(16), .DMD_SIZE(16), .RF_DATASIZE(16), .ADDRESS_WIDTH(4), .SIGNAL_WIDTH(3))
-	phase2_obj	(
+	core_obj	(
 				clk,
 				reset
 			);
