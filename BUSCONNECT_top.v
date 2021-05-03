4a4354974f60021697d3d97e17ecac3709d7b4f1
Sonu <83470958+SonuSunnyJohn@users.noreply.github.com>
2021-05-03 15:52:28 +0530

    Update BUSCONNECT_top.v

--- BUSCONNECT_top.v
+++ BUSCONNECT_top.v
@@ -1,11 +1,11 @@
-module buscnct(clk,ps_bc_drr_sclt, ps_bc_di_sclt,dm_bc_dt, dg_bc_dt, ps_bc_dt, xb_dtx, ps_bc_immdt,bc_dt);
+module BC_top(clk,ps_bc_drr_sclt, ps_bc_di_sclt,dm_bc_dt, dg_bc_dt, ps_bc_dt, xb_dtx, ps_bc_immdt,bc_dt);
 
 input clk;
 input[1:0] ps_bc_drr_sclt, ps_bc_di_sclt;
 input[15:0] dm_bc_dt, dg_bc_dt, ps_bc_dt, xb_dtx, ps_bc_immdt;
 output[15:0] bc_dt;
 
-reg[15:0] bc_dt,ps_bc_drr_dt,bc_dt;
+reg[15:0] bc_dt,ps_bc_drr_dt,bc_pdrdt;
 
 always @(*) begin
 
@@ -22,7 +22,7 @@ end
 
 always @ (posedge clk) begin
 
-	bc_dt<= ps_bc_drr_dt;
+	bc_pdrdt<= ps_bc_drr_dt;
 
 end
 
@@ -31,7 +31,7 @@ always @(*) begin
 	if(ps_bc_di_sclt== 2'b0)
 		bc_dt= dm_bc_dt;
 	else if(ps_bc_di_sclt== 2'b01)
-		bc_dt= bc_dt;
+		bc_dt= bc_pdrdt;
 	else if(ps_bc_di_sclt== 2'b10)
 		bc_dt= ps_bc_immdt;
 	else
