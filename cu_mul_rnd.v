853dc73983a679cbab32eeaf6af51e176e6d48d0
Ashwin Pradeep <ashwinspradeep@gmail.com>
2021-06-13 04:46:18 +0530

    round after accumulation update
    rounding done on 40 bit mul value now.

--- cu_mul_rnd.v
+++ cu_mul_rnd.v
@@ -2,9 +2,9 @@
 module mul_rnd	
 		#(parameter SIZE=16)
 		(
-			input wire[2*SIZE-1:0] data_in,		//mul32_product
+			input wire[SIZE*5/2-1:0] data_in,		//mul40_out_data
 			input wire ps_mul_rndPrdt,
-			output reg[2*SIZE-1:0] out		//rnd32_out
+			output reg[SIZE*5/2-1:0] out		//rnd40_out
 		);
 	
 	always@(*)
@@ -13,15 +13,15 @@ module mul_rnd
 		begin
 			if(~data_in[SIZE-1])	
 				//truncate
-				out = { data_in[2*SIZE-1:SIZE], {SIZE{1'b0}} };
+				out = { data_in[SIZE*5/2-1:SIZE], {SIZE{1'b0}} };
 			else
 			begin
 				if(data_in[SIZE-2:0]=={(SIZE-1){1'b0}})
 					//rnd to make data[16]=0 and truncate lsb16
-					out = { (data_in[2*SIZE-1:SIZE]+data_in[SIZE]), {SIZE{1'b0}} };
+					out = { (data_in[SIZE*5/2-1:SIZE]+data_in[SIZE]), {SIZE{1'b0}} };
 				else
 					//add 1 to msb16 and truncate lsb16
-					out = { (data_in[2*SIZE-1:SIZE]+1), {SIZE{1'b0}} };
+					out = { (data_in[SIZE*5/2-1:SIZE]+1), {SIZE{1'b0}} };
 			end
 		end
 		
