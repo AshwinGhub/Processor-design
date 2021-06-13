2268549fbb25e7d749e78a30d14c4ac8df6e85ba
arundhathy <arundhathy2014@gmail.com>
2021-06-13 22:07:25 +0530

    after revert

--- test_core.v
+++ test_core.v
@@ -7,8 +7,8 @@ reg[9:0] timeout;
 integer i;
 
 core_top #(.PMA_SIZE(16), .PMD_SIZE(32), .DMA_SIZE(16), .DMD_SIZE(16), .RF_DATASIZE(16), .ADDRESS_WIDTH(4), .SIGNAL_WIDTH(3), 
-			.PM_LOCATE("C:/Users/Ashwin Pradeep/Desktop/Project Final Year/GIT repo/memory_files/pm_file.txt"), 
-			.DM_LOCATE("C:/Users/Ashwin Pradeep/Desktop/Project Final Year/GIT repo/memory_files/dm_file.txt")
+			.PM_LOCATE("C:/Users/Dell/Desktop/Arundhathy-files/Processor-design/memory_files/pm_file.txt"), 
+			.DM_LOCATE("C:/Users/Dell/Desktop/Arundhathy-files/Processor-design/memory_files/dm_file.txt")
 			)
 	core_obj	(
 				clk,
